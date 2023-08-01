a9f1cddd454a365346f9a889cef3b60d0deb8252
M Za'im S <129059018+hwangzie@users.noreply.github.com>
2023-08-01 16:04:20 +0700

    Add files via upload

--- pertemuan-1/duplikas-googlesearch/script.js
+++ pertemuan-1/duplikas-googlesearch/script.js
@@ -1,5 +1,7 @@
 let section = document.getElementById("section");
-section.style.textAlign = "center";
+section.style.display = "flex";
+section.style.alignItems = "center";
+section.style.flexDirection = "column";
 
 const h1 = document.createElement("h1");
 h1.innerHTML = "Google";
@@ -16,16 +18,27 @@ input.style.border = "2px solid #ccc";
 input.style.borderRadius = "10rem";
 input.style.width = "50%"
 
-// const but1 = document.createElement("button");
-// but1.innerHTML = "i feel lucky"
-// but1.style.margin = "10px";
-// but1.style.padding = "10px";
-// but1.style.borderRadius = "14px";
-// but1.style.border = "1px solid #ccc";
-// but1.style.flexDirection = "collumn"
+const but1 = document.createElement("button");
+but1.innerHTML = "i feel lucky"
+but1.style.margin = "10px";
+but1.style.padding = "10px";
+but1.style.borderRadius = "14px";
+but1.style.border = "1px solid #ccc";
 
+const but2 = document.createElement("button");
+but2.innerHTML = "i feel lucky"
+but2.style.margin = "10px";
+but2.style.padding = "10px";
+but2.style.borderRadius = "14px";
+but2.style.border = "1px solid #ccc";
+
+const divbut = document.createElement("div");
+divbut.style.display = "flex";
+divbut.style.flexDirection = "row";
 
 section.append(h1);
 section.append(input);
-section.append(but1);
+section.append(divbut);
+divbut.append(but1);
+divbut.append(but2);
 console.log(h1)
